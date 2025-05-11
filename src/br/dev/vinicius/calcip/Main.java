6e4ada72883d3f28fcf78c0c6f5c79a15fd11d42
Vinicius <vinicius.oliveira100307@gmail.com>
2025-05-10 23:44:12 -0300

    saindo do 0

---
+++ src/br/dev/vinicius/calcip/Main.java
@@ -0,0 +1,20 @@
+package br.dev.vinicius.calcip;
+
+import br.dev.vinicius.calcip.model.IPv4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+//		  String texto = "192.168.0.3/25";
+//	        String[] partes = texto.split("[.\\/]");
+//
+//	        for (String parte : partes) {
+//	            System.out.println(parte);
+//	        }
+
+	}
+
+}
---
+++ src/br/dev/vinicius/calcip/model/IPv4.java
@@ -0,0 +1,29 @@
+package br.dev.vinicius.calcip.model;
+
+
+
+public class IPv4 {
+    String ip;
+    
+    //Divide o Ip em partes usando o ponto como separador
+    String[] octetos = ip.split("\\.");
+    
+    //Criando as variaveis para guardar cada octeto 
+    private String oc1 = octetos[0];
+    private String oc2 = octetos[1];
+    private String oc3 = octetos[2];
+    private String oc4 = octetos[3];
+    
+    //convertendo o Oc1 para int 
+    int validOc1 = Integer.parseInt(octetos[0]);
+    
+	
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	
+	public String getIp() {
+		return ip;
+	}
+	
+}
