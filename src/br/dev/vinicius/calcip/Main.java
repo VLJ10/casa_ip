a55c65e660daf13b70120755403924e498a2ff04
Vinicius <vinicius.oliveira100307@gmail.com>
2025-05-22 23:53:11 -0300

    20% do caminho andado

--- src/br/dev/vinicius/calcip/Main.java
+++ src/br/dev/vinicius/calcip/Main.java
@@ -7,14 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		
-//		  String texto = "192.168.0.3/25";
-//	        String[] partes = texto.split("[.\\/]");
-//
-//	        for (String parte : partes) {
-//	            System.out.println(parte);
-//	        }
-
 	}
 
 }
--- src/br/dev/vinicius/calcip/model/IPv4.java
+++ src/br/dev/vinicius/calcip/model/IPv4.java
@@ -1,29 +1,56 @@
 package br.dev.vinicius.calcip.model;
 
-
+import java.io.IOException;
 
 public class IPv4 {
-    String ip;
-    
-    //Divide o Ip em partes usando o ponto como separador
-    String[] octetos = ip.split("\\.");
-    
-    //Criando as variaveis para guardar cada octeto 
-    private String oc1 = octetos[0];
-    private String oc2 = octetos[1];
-    private String oc3 = octetos[2];
-    private String oc4 = octetos[3];
-    
-    //convertendo o Oc1 para int 
-    int validOc1 = Integer.parseInt(octetos[0]);
-    
-	
+	private String ip;
+	private String[] subMask;
+	private String[] octetos;
+
 	public void setIp(String ip) {
 		this.ip = ip;
+		
+		this.subMask = ip.split("/");
+		this.octetos = subMask[0].split("\\.");
 	}
-	
+
 	public String getIp() {
 		return ip;
 	}
-	
+
+	// Criando as variaveis para guardar cada octeto
+	private String oc1 = octetos[0];
+	private String oc2 = octetos[1];
+	private String oc3 = octetos[2];
+	private String oc4 = octetos[3];
+
+	// convertendo o Oc1 para int
+	public void verificarClasseIp() {
+		if (octetos == null || octetos.length != 4) {
+            System.out.println("Endereço IP inválido");
+            return;
+        }
+		
+		try {
+			int classeDoIp = Integer.parseInt(oc1);
+			
+			if (classeDoIp < 127) {
+				System.out.print("IP classe A");
+			} else if(classeDoIp >= 128 && classeDoIp <= 191){
+				System.out.print("IP classe B");
+			} else if(classeDoIp >= 192 && classeDoIp <= 223){
+				System.out.print("IP classe C");
+			} else {
+				System.out.print("Esse endereço de ip é reservado para outras atividades");
+			}
+			
+		} catch (NullPointerException erro) {
+			System.out.print(erro.getMessage());
+		} catch (Exception erro) {
+			System.out.print(erro.getMessage());
+		}
+	}
+
 }
+
+
