cdc09900b381540848c4fc2f8ef4a63c02be0e16
Vinicius <vinicius.oliveira100307@gmail.com>
2025-06-05 00:56:15 -0300

    Função quantidade de Ip implementada

--- src/br/dev/vinicius/calcip/Main.java
+++ src/br/dev/vinicius/calcip/Main.java
@@ -7,14 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		IPv4 ip = new IPv4();
-		ip.setIp("10.0.0.1/16");
+		ip.setIp("10.0.0.1/8");
 		ip.getSeparMascara();
 		ip.getOctetos();
 		String sub = ip.binSubMask();
 		String dec = ip.decimalSubMask();
+		String quant = ip.ipsDisponiveis();
 		
 		System.out.println(sub);
 		System.out.println(dec);
+		System.out.println(quant);
 	}
 
 }
